d73f206cef350bea08abd1c24568759274844fb0
Adrian Dapprich <adrian@dappri.ch>
2023-10-02 18:28:25 +0900

    finally seems to work
    We want to put some static variables in a section .wifi_info.
    For that we use a linker script memory.x. At first I tried to make the
    size 256 bytes. But the initial values from the compiled program were
    not being written (although there was a UF2 block for it). Inceasing the
    size of the section to the flash sector size 4096 bytes seems to
    work (but not if it's at the end of flash, have to test that again.) No
    I'm just using an address at the far end of the address space.

--- pico/memory.x
+++ pico/memory.x
@@ -1,5 +1,13 @@
 MEMORY {
     BOOT2 : ORIGIN = 0x10000000, LENGTH = 0x100
-    FLASH : ORIGIN = 0x10000100, LENGTH = 2048K - 0x100
+    FLASH : ORIGIN = 0x10000100, LENGTH = 2048K - 0x1100
+    WIFI_INFO : ORIGIN = 0x10ff0000, LENGTH = 0x1000
     RAM   : ORIGIN = 0x20000000, LENGTH = 256K
 }
+
+SECTIONS {
+     .wifi_info :  {
+       *(.wifi_info);
+       . = ALIGN(4);
+     } > WIFI_INFO
+} INSERT AFTER .text;
--- pico/src/static_data.rs
+++ pico/src/static_data.rs
@@ -9,7 +9,9 @@ pub static DEVICE_ID: u8 = 0;
 // #[link_section = ".wifi_info"]
 // static WIFI_PW_BYTES: [u8; 32] = *b"mysecretpw\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
 
+#[link_section = ".wifi_info"]
 static WIFI_SSID_BYTES: [u8; 32] = *b"TP-Link_0FFC\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+#[link_section = ".wifi_info"]
 static WIFI_PW_BYTES: [u8; 32] = *b"70667103\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
 // static WIFI_SSID_BYTES: [u8; 32] = *b"Buffalo-G-1337\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
 // static WIFI_PW_BYTES: [u8; 32] = *b"mysecretpw\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
@@ -17,35 +19,56 @@ static WIFI_PW_BYTES: [u8; 32] = *b"70667103\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0
 pub static SERVER_ENDPOINT: IpEndpoint = IpEndpoint::new(IpAddress::v4(192, 168, 0, 194), 1337);
 
 pub fn wifi_ssid() -> Option<&'static str> {
-    let cstr = match CStr::from_bytes_until_nul(&WIFI_SSID_BYTES) {
-        Ok(cstr) => cstr,
-        Err(e) => {
-            log::error!("Parsing Wifi SSID failed: {}", e);
-            return None;
-        }
-    };
-    match cstr.to_str() {
+    let end = WIFI_SSID_BYTES
+        .iter()
+        .position(|b| *b == 0)
+        .unwrap_or_else(|| WIFI_SSID_BYTES.len());
+
+    log::info!(
+        "SSID: {:x}{:x}{:x}",
+        WIFI_SSID_BYTES[0],
+        WIFI_SSID_BYTES[1],
+        WIFI_SSID_BYTES[2]
+    );
+
+    match core::str::from_utf8(&WIFI_SSID_BYTES[..end]) {
         Ok(wifi_ssid) => Some(wifi_ssid),
         Err(e) => {
             log::error!("Parsing Wifi SSID failed: {}", e);
             None
         }
     }
+
+    // let cstr = match CStr::from_bytes_until_nul(&WIFI_SSID_BYTES) {
+    //     Ok(cstr) => cstr,
+    //     Err(e) => {
+    //         log::error!("Parsing Wifi SSID failed: {}", e);
+    //         return None;
+    //     }
+    // };
+    // match cstr.to_str() {
+    //     Ok(wifi_ssid) => Some(wifi_ssid),
+    //     Err(e) => {
+    //         log::error!("Parsing Wifi SSID failed: {}", e);
+    //         None
+    //     }
+    // }
 }
 
 pub fn wifi_password() -> Option<&'static str> {
-    let cstr = match CStr::from_bytes_until_nul(&WIFI_PW_BYTES) {
-        Ok(cstr) => cstr,
-        Err(e) => {
-            log::error!("Parsing Wifi password failed: {}", e);
-            return None;
-        }
-    };
-    match cstr.to_str() {
-        Ok(wifi_pw) => Some(wifi_pw),
-        Err(e) => {
-            log::error!("Parsing Wifi password failed: {}", e);
-            None
-        }
-    }
+    return Some("70667103");
+    // let cstr = match CStr::from_bytes_until_nul(&WIFI_PW_BYTES) {
+    //     Ok(cstr) => cstr,
+    //     Err(e) => {
+    //         log::error!("Parsing Wifi password failed: {}", e);
+    //         return None;
+    //     }
+    // };
+    // match cstr.to_str() {
+    //     Ok(wifi_pw) => Some(wifi_pw),
+    //     Err(e) => {
+    //         log::error!("Parsing Wifi password failed: {}", e);
+    //         None
+    //     }
+    // }
 }
