84d41d9bd02398b272308ffe4d93fb89eae3985e
Adrian Dapprich <adrian@dappri.ch>
2023-10-04 23:27:09 +0900

    update script to generate UF2 for device info section

--- create_wifi_uf2.py
+++ create_wifi_uf2.py
@@ -1,12 +1,14 @@
 import struct 
 
-ssid = 'Buffalo-G-1337'.encode('utf-8')
-password = 'mysecretpw'.encode('utf-8')
+device_id = 0xcafebabe
+ssid = 'Buffalo-G-1338'.encode('utf-8')
+password = 'mysecretpw2'.encode('utf-8')
 
 assert (len(ssid) <= 32)
 assert (len(password) <= 32)
 
-base_address = 0x10fff000
+wifi_base_address = 0x10fff000
+device_base_address = 0x10ffe000
 
 def gen_block(address, block_id, data):
     assert(len(data) == 256)
@@ -19,13 +21,13 @@ def gen_block(address, block_id, data):
     # flags (familyID present)
     f += b'\x00\x20\x00\x00'
     # address where it should be written
-    f += struct.pack('<i', address)
+    f += struct.pack('<I', address)
     # size of block (256)
     f += b'\x00\x01\x00\x00'
     # sequential block number
-    f += struct.pack('<i', block_id)
+    f += struct.pack('<I', block_id)
     # total number of blocks
-    f += struct.pack('<i', 16)
+    f += struct.pack('<I', 16)
     # familyID
     f += b'\x56\xff\x8b\xe4'
     f += data
@@ -36,16 +38,32 @@ def gen_block(address, block_id, data):
 
     return f
 
+def gen_wifi():
+    wifi_data = ssid + (32 - len(ssid)) * b'\0'
+    wifi_data += password + (32 - len(password)) * b'\0'
+    wifi_data +=  (256 - 64) * b'\0'
 
-wifi_data = ssid + (32 - len(ssid)) * b'\0'
-wifi_data += password + (32 - len(password)) * b'\0'
-wifi_data +=  (256 - 64) * b'\0'
+    file = b''
+    file += gen_block(wifi_base_address, 0, wifi_data)
 
-file = b''
-file += gen_block(base_address, 0, wifi_data)
+    for i in range(1, 16):
+        file += gen_block(wifi_base_address + 256 * i, i, 256 * b'\0')
 
-for i in range(1, 16):
-    file += gen_block(base_address + 256 * i, i, 256 * b'\0')
-
-with open('./wifi.uf2', 'wb') as f:
-    f.write(file)
+    with open('./wifi.uf2', 'wb') as f:
+        f.write(file)
+        
+def gen_dev():
+    device_data = struct.pack('<I', device_id)
+    device_data += (256 - 4) * b'\0'
+    
+    file = b''
+    file += gen_block(device_base_address, 0, device_data)
+    
+    for i in range(1, 16):
+        file += gen_block(device_base_address + 256 * i, i, 256 * b'\0')
+        
+    with open('./dev.uf2', 'wb') as f:
+        f.write(file)
+    
+gen_wifi()
+gen_dev()
