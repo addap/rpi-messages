98604128365d67ff12179c9fbaf86cbb33eadcf9
Adrian Dapprich <adrian@dappri.ch>
2025-03-19 07:40:22 +0100

    Send constructed form data.

--- server/webclient/index.js
+++ server/webclient/index.js
@@ -1,23 +1,31 @@
 
 function submitMultipart() {
+  console.log("Calling event listener of mp submit");
   const fileIpt = document.getElementById("mp-file");
   const receiverIpt = document.getElementById("mp-receiver");
   const durationIpt = document.getElementById("mp-duration");
 
-  const formData = FormData();
+  const formData = new FormData();
   const file = fileIpt.files[0];
   formData.append("image", file);
   formData.append("receiver", receiverIpt.value);
-  formData.append("duration", durationIpt.value);
-  // formData.append("meta", {
-  //   "rece"
-  // })
+  formData.append("duration", durationIpt.value * 60 * 60);
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "/mp_new_image_message", true);
+
+  xhr.onload = () => {
+    console.log("Finished sending form data.");
+  }
+  xhr.send(formData);
 }
 
 
-document.addEventListener("load", () => {
+window.addEventListener("load", () => {
+  console.log("Loaded document");
   const submitBtn = document.getElementById("mp-submit");
   if (submitBtn) {
+    console.log("Adding event listener to mp submit");
     submitBtn.addEventListener("click", submitMultipart);
   }
 });
