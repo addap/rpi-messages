6b33a400d753d9e0c4ca338dec72de3e49f627c4
Adrian Dapprich <adrian@dappri.ch>
2025-03-19 07:44:44 +0100

    try putting form in formdata constructor directly.
    Works but accessing all fields for now is easier.

--- server/webclient/index.js
+++ server/webclient/index.js
@@ -1,11 +1,14 @@
 
 function submitMultipart() {
   console.log("Calling event listener of mp submit");
+  // TODO is possible to fill directly from form but then we need to attach a "formdata" event listener to the form to change the hours to seconds
+  // also how do we do validation on it?
+  // const form = document.getElementById("mp-form");
   const fileIpt = document.getElementById("mp-file");
   const receiverIpt = document.getElementById("mp-receiver");
   const durationIpt = document.getElementById("mp-duration");
 
-  const formData = new FormData();
+  // const formData = new FormData();
   const file = fileIpt.files[0];
   formData.append("image", file);
   formData.append("receiver", receiverIpt.value);
